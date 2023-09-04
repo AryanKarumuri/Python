bc5505e8e8a22866ffceb8a131fa383fe2f4b4fe
AryanKarumuri <109222300+AryanKarumuri@users.noreply.github.com>
2023-09-04 22:40:34 +0530

    empty class

---
+++ 41.class.py
@@ -0,0 +1,9 @@
+#Defining a new class member
+class Member: 
+    """Empty class"""
+    pass
+obj = Member()
+print(obj)
+
+#Output: <__main__.Member object at 0x000001F56930FA90>
+#obj is an object created from Member class. The number at the end is location in memory.
